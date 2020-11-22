6c1e36fa4eca8a6f88cdf89b5f83d81d91ddd25c
Yathuthurai <50237091+Yathuthurai@users.noreply.github.com>
2020-11-22 19:28:01 +0530

    Added templating engine - ejs

--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,28 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../utils/path");
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
+
+// router.get("/", (req, res, next) => {
+//     const products = adminData.products;
+//     res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
+//     // console.log('shop.js', adminData.products);
+//     // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+//   });
