5ee070cc5b3efcaf4d948167b5adad55ff6a3c12
Yathuthurai <50237091+Yathuthurai@users.noreply.github.com>
2020-12-05 21:40:39 +0530

    Implemented Authentication flow

--- app.js
+++ app.js
@@ -70,16 +70,6 @@ app.use((req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "Yathu",
-          email: "yathu@test.com",
-          cart: { items: [] },
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch((err) => {
