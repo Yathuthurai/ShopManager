072d6dc067598734c8ef472a1fd39690001496d4
Yathuthurai <50237091+Yathuthurai@users.noreply.github.com>
2020-12-06 09:51:37 +0530

    Adding csrf protection

--- app.js
+++ app.js
@@ -50,17 +50,11 @@ app.use((req, res, next) => {
     .catch((err) => console.log(err));
 });
 
-// app.use((req, res, next) => {
-//   if (!req.session.user) {
-//     return next();
-//   }
-//   User.findById(req.session.user._id)
-//     .then((user) => {
-//       req.user = user;
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
