35cbb8c3ca8890f83e06fd9bee08cd807bfdf0ac
Yathuthurai <50237091+Yathuthurai@users.noreply.github.com>
2020-12-06 10:29:19 +0530

    Providing user feedback

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,11 +2,16 @@ const bycrypt = require("bcryptjs");
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-  //   const isLoggedIn = req.get("Cookie").split(";")[1].trim().split("=")[1];
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: false,
+    errorMessage: message,
   });
 };
 
@@ -14,7 +19,6 @@ exports.getSignup = (req, res, next) => {
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
-    isAuthenticated: false,
   });
 };
 
@@ -24,6 +28,7 @@ exports.postLogin = (req, res, next) => {
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
+        req.flash("error", "Invalid email or password!");
         return res.redirect("/login");
       }
       bycrypt
